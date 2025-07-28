438760461336124fea37c4dd14f2a2bb5413825b
Pradeep Saran <pradeepmadasar@gmail.com>
2025-07-28 14:59:51 +0530

    once again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Calculator from './calculator/calculator.js';
 import Xlogin from './Login/login.js';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';    
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';    
 
 function App() {
   return (
     <div className="App">   
        <Router>
+        <nav style={{ padding: '10px', background: '#eee' }}>
+          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          <Link to="/login">XLogin</Link>
+        </nav>
+
         <Routes>
           <Route path="/" element={<Calculator />} />
           <Route path="/login" element={<Xlogin />} />
