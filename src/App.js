dd1b2ff7aaf9ce419734d8a7bfb0003b978f0f01
Pradeep Saran <pradeepmadasar@gmail.com>
2025-07-28 14:55:13 +0530

    add something new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Calculator from './calculator.js';
+import Calculator from './calculator/calculator.js';
+import Xlogin from './Login/login.js';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';    
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
+    <div className="App">   
+       <Router>
+        <Routes>
+          <Route path="/" element={<Calculator />} />
+          <Route path="/login" element={<Xlogin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
