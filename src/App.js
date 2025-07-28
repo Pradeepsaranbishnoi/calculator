cc6e957e80d6a1bddf0d76b0d043451a60684f62
Pradeep Saran <pradeepmadasar@gmail.com>
2025-07-28 15:20:31 +0530

    stopwatch add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Calculator from './calculator/calculator.js';
+import Stopwatch from './Stopwatch/Stopwatch.js';
 import Xlogin from './Login/login.js';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';    
@@ -9,12 +10,14 @@ function App() {
        <Router>
         <nav style={{ padding: '10px', background: '#eee' }}>
           <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-          <Link to="/login">XLogin</Link>
+          <Link to="/login" style={{ marginRight: '10px' }}>XLogin</Link>
+          <Link to="/stopwatch" style={{ marginRight: '10px' }}>Stopwatch</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Calculator />} />
           <Route path="/login" element={<Xlogin />} />
+          <Route path="/stopwatch" element={<Stopwatch />} />
         </Routes>
       </Router>
     </div>
